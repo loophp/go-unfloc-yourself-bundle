486a8808d9b0cc4fcd91a07e040f06d238ec0d66
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-04-27 22:28:18 +0200

    tests: Fix minor things.

--- src/Tests/Functional/FooControllerTest.php
+++ src/Tests/Functional/FooControllerTest.php
@@ -7,7 +7,7 @@
 
 declare(strict_types=1);
 
-namespace loophp\GoUnflocYourselfBundle\Tests\Functional\Controller;
+namespace loophp\GoUnflocYourselfBundle\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
